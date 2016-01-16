dc6b4af6aea239fc44ff7bc35210b0081165dae5
TudorRotarus25 <tudor.rotarus@gmail.com>
2016-01-16 10:18:02 +0200

    Added all files

--- meow/meow/wsgi.py
+++ meow/meow/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for meowbook project.
+WSGI config for meow project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meowbook.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meow.settings")
+
 application = get_wsgi_application()
