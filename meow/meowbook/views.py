527f125b312309063396362ced2121b6a3c4f0b1
TudorRotarus25 <tudor.rotarus@gmail.com>
2016-01-16 10:51:00 +0200

    News Feed view created

--- meow/meowbook/views.py
+++ meow/meowbook/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.list import ListView
+
+
+class NewsFeedView(ListView):
+    model = CatPictures
+    template_name = 'NewsFeed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsFeedView, self).get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        return context
